49b58d98b9f036ae7671ef8ca9455621b3f7308f
Takayuki Kaburagi <t.kaburagi@me.com>
2020-02-19 11:56:22 +0900

    modified to use docker

--- run.sh
+++ run.sh
@@ -8,10 +8,4 @@ cd ..
 
 docker-compose build --no-cache
 
-consul agent -server -bind=0.0.0.0 \
--client=0.0.0.0 \
--data-dir=${DATA_DIR} \
--bootstrap-expect=1 -ui \
--config-dir=${CONFIG_DIR}/consul-config.hcl &
-
 docker-compose up
