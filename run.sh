a0fab371b10f2d73c4e32becc6dc899b7eeec3d3
Takayuki Kaburagi <t.kaburagi@me.com>
2020-02-19 13:51:19 +0900

    modified to use docker

--- run.sh
+++ run.sh
@@ -6,6 +6,11 @@ cd web-backend-consul
 
 cd ..
 
+consul agent -server -bind=127.0.0.1 \
+-client=0.0.0.0 \
+-bootstrap-expect=1 -ui \
+-config-dir=./consul.d &
+
 docker-compose build --no-cache
 
 docker-compose up
